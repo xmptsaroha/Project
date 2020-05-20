c420ec488597ccf260911c4b3cb07d030eedee85
Shubham Saroha <shusaroha96@gmail.com>
2020-05-20 13:24:02 +0530

    Recipe Model

---
+++ src/app/recipes/recipe.model.ts
@@ -0,0 +1,12 @@
+//Defines the model for the Recipe
+export class Recipe {
+    public name: string;
+    public description: string;
+    public imagePath: string;
+
+    constructor(name: string, desc: string, imagePath: string) {
+        this.name = name;
+        this.description = desc;
+        this.imagePath = imagePath;
+    }
+}
