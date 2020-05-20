13acffc751474f600ef314369cf82b45c90bc6fa
Shubham Saroha <shusaroha96@gmail.com>
2020-05-20 13:49:06 +0530

    Recipe Component

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,8 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes = [];
-  
+  recipes: Recipe[] = [
+    new Recipe('A test Recipe', 'This is simply a test', 'https://cdn.pixabay.com/photo/2014/12/21/23/28/recipe-575434_1280.png'),
+    new Recipe('Second Recipe', 'This is Second test', 'https://cdn.pixabay.com/photo/2017/04/08/14/37/kitchen-2213422_1280.jpg'),
+    new Recipe('Third Recipe', 'This is Third test', 'https://c.pxhere.com/photos/b0/e6/food_snacks_cooking_pork_meat_seasoning_wine_side_dishes_republic_of_korea-1410325.jpg!d')
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
